fe035a27997f2da06f3749ce7f4c27569ac57634
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-08-26 20:29:08 -0600

    Added gitignore

--- favorite_books/settings.py
+++ favorite_books/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'si(mg+3(o1_6ewwa^0fambxa!!krh8g^_u5%e#1vhdu)l2px52'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG_STATUS"))
 
 ALLOWED_HOSTS = []
 
